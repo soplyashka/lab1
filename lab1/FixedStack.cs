644ef216d8c47540f2364c0da8b30cb68f3266b3
Anastasiya Safrygina <safrygina.a.n@edu.mirea.ru>
2021-11-27 10:07:25 +0300

    без счетчика

---
+++ lab1/FixedStack.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab1
+{
+    public class FixedStack<T>
+    {
+        private List<int> stack = new List<int>();
+        
+        
+        // размер стека
+        public int Count()
+        {
+             return stack.Count; 
+        }
+        // добвление элемента
+        public void Push(int item)
+        {
+            stack.Add(item);
+        }
+        // извлечение элемента
+        public int Pop()
+        {
+            int result = stack[stack.Count - 1];
+            stack.RemoveAt(stack.Count - 1);
+
+            return result;
+        }
+        public int Get(int pos)
+        {
+            Stack<int> tmp = new Stack<int>();
+            for (int i = Count() - 1; i > pos; i--)
+            {
+                tmp.Push(Pop());
+            }
+            int Result = stack[Count() - 1];
+            for (int i = tmp.Count() - 1; i >= 0; i--)
+            {
+                Push(tmp.Pop());
+            }
+            return Result;
+        }
+
+        public void Set(int pos, int value)
+        {
+            Stack<int> tmp = new Stack<int>();
+            for (int i = Count() - 1; i > pos; i--)
+            {
+                tmp.Push(Pop());
+            }
+            stack[Count() - 1] = value;
+            for (int i = tmp.Count() - 1; i >= 0; i--)
+            {
+                Push(tmp.Pop());
+            }
+        }
+        public void Print()
+        {
+            foreach (int item in stack)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+
+}
--- lab1/laba1.cs
+++ lab1/laba1.cs
@@ -10,108 +10,34 @@ namespace lab1
 {
     class Program
     {
-
+        // Массив, стэк, пузырьковая - 78
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello,Leonid!");
-            FixedStack<int> myStack = new FixedStack<int>(5);
-            myStack.Push(1);
-            myStack.Push(2);
-            myStack.Push(15);
-            myStack.Push(77);
-            myStack.Push(3);
-            myStack.Pop();
-            int[] copyStack = new int[myStack.Count];
-            Console.WriteLine($"Количество элементов в стэке: {myStack.Count}");
-            Program.BubbleSort(myStack.items);
-            Console.ReadLine();
-
-
-        }
-        public static void BubbleSort(int[]nums)
-        {
-            // сортировка
-            int temp;
-            for (int i = 0; i < nums.Length - 1; i++) // Во внешнем цикле мы берем элемент, который будем сравнивать:
+            FixedStack<int> myStack = new();
+            Random rnd = new Random();
+            for (int i = 1; i <= 10; i++)
             {
-                for (int j = i + 1; j < nums.Length; j++) //запускаем вложенный цикл, который начинается, со следующего элемента:
+                myStack.Push(rnd.Next(1, 100));
+            }
+            myStack.Print();
+            int temp;
+            for (int i = 0; i < myStack.Count() - 1; i++) // Во внешнем цикле мы берем элемент, который будем сравнивать:
+            {
+                for (int j = i + 1; j < myStack.Count(); j++) //запускаем вложенный цикл, который начинается, со следующего элемента:
                 {
-                    if (nums[i] > nums[j]) //Если элемент с меньшим индексом больше элемента с большим индексом, то меняем элементы местами.
+                    if (myStack.Get(i) > myStack.Get(j)) //Если элемент с меньшим индексом больше элемента с большим индексом, то меняем элементы местами.
                     {
-                        temp = nums[i];
-                        nums[i] = nums[j];
-                        nums[j] = temp;
+                        temp = myStack.Get(i);
+                        myStack.Set(i,myStack.Get(j));
+                        myStack.Set(j,temp);
                     }
                 }
             }
-
-            // вывод
-            Console.WriteLine("Вывод отсортированного массива");
-            for (int i = 0; i < nums.Length; i++)
-            {
-                Console.WriteLine(nums[i]);
-            }
+            Console.WriteLine("Отсортированный массив: ");
+            myStack.Print();
+            Console.ReadLine();
         }
-        public class FixedStack<T>
-        {
-            public int[] items; // элементы стека
-            private int count;  // количество элементов
-            const int n = 10;   // количество элементов в массиве по умолчанию
-            public FixedStack()
-            {
-                items = new int[n];
-            }
-            public FixedStack(int length)
-            {
-                items = new int[length];
-            }
-            // пуст ли стек
-            public bool IsEmpty
-            {
-                get { return count == 0; }
-            }
-            // размер стека
-            public int Count
-            {
-                get { return count; }
-            }
-            // добвление элемента
-            public void Push(int item)
-            {
-                // если стек заполнен, выбрасываем исключение
-                if (count == items.Length)
-                    throw new InvalidOperationException("Переполнение стека");
-                items[count++] = item;
-            }
-            // извлечение элемента
-            public int Pop()
-            {
-                // если стек пуст, выбрасываем исключение
-                if (IsEmpty)
-                    throw new InvalidOperationException("Стек пуст");
-                int item = items[--count];
-                items[count] = default(int); // сбрасываем ссылку
-                return item;
-            }
-            // возвращаем элемент из верхушки стека
-            public int Peek()
-            {
-                // если стек пуст, выбрасываем исключение
-                if (IsEmpty)
-                    throw new InvalidOperationException("Стек пуст");
-                return items[count - 1];
-            }
-
-            private void Resize(int max)
-            {
-                int[] tempItems = new int[max];
-                for (int i = 0; i < count; i++)
-                    tempItems[i] = items[i];
-                items = tempItems;
-            }
-        }
+        
     }
-    
-    
 }
 
