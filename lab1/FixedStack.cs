1afe569a1507dfe016a95a7ec508e3b97b07d1e7
Anastasiya Safrygina <safrygina.a.n@edu.mirea.ru>
2021-12-05 16:08:54 +0300

    +счетчик

--- lab1/FixedStack.cs
+++ lab1/FixedStack.cs
@@ -6,55 +6,75 @@ using System.Threading.Tasks;
 
 namespace lab1
 {
-    public class FixedStack<T>
+    public class FixedStack
     {
-        private List<int> stack = new List<int>();
-        
-        
+        public int N_op = 0;
+        private List<int> stack = new List<int>(); 
         // размер стека
         public int Count()
         {
-             return stack.Count; 
+             return stack.Count; //2
+             
         }
         // добвление элемента
-        public void Push(int item)
+        public void Push(int item) //2
         {
-            stack.Add(item);
+            stack.Add(item); //2
+            N_op += 2;
         }
         // извлечение элемента
-        public int Pop()
+        public int Pop() //5
         {
-            int result = stack[stack.Count - 1];
-            stack.RemoveAt(stack.Count - 1);
+
+            int result = stack[stack.Count - 1]; //3
+            stack.RemoveAt(stack.Count - 1); //2
+            N_op += 5;
 
             return result;
         }
         public int Get(int pos)
         {
-            Stack<int> tmp = new Stack<int>();
+            FixedStack tmp = new FixedStack(); N_op += 2;
+            int result = 0; N_op += 2;
             for (int i = Count() - 1; i > pos; i--)
             {
+                N_op++;
                 tmp.Push(Pop());
+                N_op +=8;
             }
-            int Result = stack[Count() - 1];
+            if (Count() != 0)
+            {
+                result = stack[Count() - 1]; N_op += 3;
+            }
+
             for (int i = tmp.Count() - 1; i >= 0; i--)
             {
+                N_op++;
                 Push(tmp.Pop());
+                N_op += 8;
             }
-            return Result;
+
+            return result;
         }
+
 
         public void Set(int pos, int value)
         {
-            Stack<int> tmp = new Stack<int>();
+            FixedStack tmp = new FixedStack(); N_op += 2;
             for (int i = Count() - 1; i > pos; i--)
             {
+                N_op++;
                 tmp.Push(Pop());
             }
-            stack[Count() - 1] = value;
+            if (Count() != 0)
+            {
+                stack[Count() - 1] = value; N_op += 3;
+            }
             for (int i = tmp.Count() - 1; i >= 0; i--)
             {
+                N_op++;
                 Push(tmp.Pop());
+                N_op += 8;
             }
         }
         public void Print()
@@ -64,6 +84,7 @@ namespace lab1
                 Console.WriteLine(item);
             }
         }
+     
     }
 
 }
--- lab1/laba1.cs
+++ lab1/laba1.cs
@@ -1,5 +1,4 @@
-﻿
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System;
 using System.Collections;
 using System.Linq;
@@ -8,36 +7,63 @@ using System.Threading.Tasks;
 
 namespace lab1
 {
+
     class Program
     {
+
         // Массив, стэк, пузырьковая - 78
-        static void Main(string[] args)
+        public static FixedStack Sort(FixedStack myStack)
         {
-            FixedStack<int> myStack = new();
-            Random rnd = new Random();
-            for (int i = 1; i <= 10; i++)
-            {
-                myStack.Push(rnd.Next(1, 100));
-            }
-            myStack.Print();
             int temp;
-            for (int i = 0; i < myStack.Count() - 1; i++) // Во внешнем цикле мы берем элемент, который будем сравнивать:
+            for (int i = 0; i < myStack.Count() - 1; i++)
             {
-                for (int j = i + 1; j < myStack.Count(); j++) //запускаем вложенный цикл, который начинается, со следующего элемента:
+                for (int j = i + 1; j < myStack.Count(); j++)
                 {
-                    if (myStack.Get(i) > myStack.Get(j)) //Если элемент с меньшим индексом больше элемента с большим индексом, то меняем элементы местами.
+                    if (myStack.Get(i) > myStack.Get(j))
                     {
                         temp = myStack.Get(i);
-                        myStack.Set(i,myStack.Get(j));
-                        myStack.Set(j,temp);
+                        myStack.Set(i, myStack.Get(j));
+                        myStack.Set(j, temp);
                     }
                 }
             }
-            Console.WriteLine("Отсортированный массив: ");
-            myStack.Print();
-            Console.ReadLine();
+            return myStack;
         }
+        public static void Main(string[] args)
+        {
+            FixedStack myStack = new();
+            int[] mass = new int[3000];
+            Random rnd = new Random();
+            int N = 300;
+            int t_s;
+            int t_f;
+            for (int i = 0; i < 3000; i++)
+            {
+                mass[i] = rnd.Next(1, 300);
+            }
+
+            for (int i = 0; i < 10; i++)
+            {
+                for (int z = N - 300; z < N; z++)
+                {
+                    if (z < 3000)
+                    {
+                        myStack.Push(mass[z]);
+                    }
+
+                }
+                myStack.N_op = 0;
+                t_s = Environment.TickCount & Int32.MaxValue;
+                myStack = Program.Sort(myStack);
+                t_f = Environment.TickCount & Int32.MaxValue;
+                Console.WriteLine($"Номер сортировки: {i + 1} Колличество отсортированных элементов: {N}  Время сортировки(s): {t_f - t_s} Колличество операций (N_op): {myStack.N_op}");
+                N += 300;
+
+            }
+        }
+
         
     }
 }
+
 
